4454f07d5663ea51b95abf1e99fe2082e4e06e73
Kaio Silva <kaiohp.silva1@gmail.com>
2024-01-01 19:27:55 -0300

    Update the path to data folders.

--- freecurrencyapi/src/file_handle.py
+++ freecurrencyapi/src/file_handle.py
@@ -9,7 +9,7 @@ class FileHandler:
     """
     The Great docs here
     """
-    def __init__(self, local="data/raw", file_name="apidata"):
+    def __init__(self, local="freecurrencyapi/data/raw", file_name="apidata"):
 
         self.run_datetime = datetime.now().date().isoformat()
         self.local = Path(local)
@@ -27,5 +27,5 @@ class FileHandler:
 
 if __name__ == "__main__":
     test_data = json.dumps({"Test": "Sucess"})
-    file_explore = FileHandler(local="../tests/data", file_name="test_data")
+    file_explore = FileHandler(local="freecurrencyapi/tests/data", file_name="test_data")
     file_explore.save_json(test_data)
