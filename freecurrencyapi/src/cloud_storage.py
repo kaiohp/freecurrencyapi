82f4e141c5fb6c35ec3501b38e0a473023164831
Kaio Silva <kaiohp.silva1@gmail.com>
2024-01-14 20:35:14 -0300

    feature(cloud_storage): Creat the class to deal with the cloud storage interaction.

---
+++ freecurrencyapi/src/cloud_storage.py
@@ -0,0 +1,22 @@
+from google.cloud import storage
+from datetime import datetime
+import json
+
+
+class CSClient():
+    def __init__(self):
+        self.client = storage.Client()
+
+    def create_path(self):
+        now_timestamp = datetime.now()
+        year = now_timestamp.year
+        month = now_timestamp.month
+        day = now_timestamp.day
+        path = f"{str(year)}/{str(month)}/{day}.json"
+        return path
+
+    def send_json_object(self, data, destination, bucket_name):
+        bucket = self.client.get_bucket(bucket_name)
+        blob = bucket.blob(destination)
+        blob.upload_from_string(json.dumps(data), 'text/json')
+        return f"Data saved on Cloud Storage in {bucket_name}/{destination}"
