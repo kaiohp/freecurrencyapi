ddeef454755bd55213a234f8f574c73ffb660d06
Kaio Silva <kaiohp.silva1@gmail.com>
2024-01-14 21:19:53 -0300

    feature(main): Add the invocation to cloud storage client and save in the bucket.

--- freecurrencyapi/src/main.py
+++ freecurrencyapi/src/main.py
@@ -1,13 +1,30 @@
 from api_collector import FreeCurrencyCollector
 from file_handle import FileHandler
+from cloud_storage import CSClient
+from dotenv import load_dotenv, find_dotenv
+import os
 
 
-def start():
+def start(request):
+    load_dotenv(find_dotenv())
+    local_env = os.getenv("LOCAL")
     collector = FreeCurrencyCollector()
-    file_explore = FileHandler()
     api_data = collector.get_latest()
-    file_explore.save_json(api_data)
+
+    if local_env == "1":
+        file_explore = FileHandler()
+        file_explore.save_json(api_data)
+    else:
+        cloud_storage_client = CSClient()
+        currency = collector.base_currency
+        path = cloud_storage_client.create_path()
+        destination = f"{currency}/{path}"
+        return cloud_storage_client.send_json_object(
+            api_data,
+            destination=destination,
+            bucket_name='free_currency_exchange_data'
+        )
 
 
 if __name__ == "__main__":
-    start()
+    start({})
