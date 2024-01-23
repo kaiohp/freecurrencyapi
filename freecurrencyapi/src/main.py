abb1b1156641fb866ffbb03bb5e242d2f81b6b7f
Kaio Silva <kaiohp.silva1@gmail.com>
2024-01-22 22:55:27 -0300

    src(main): Update the scope to call the google client and access secret.

--- freecurrencyapi/src/main.py
+++ freecurrencyapi/src/main.py
@@ -1,30 +1,35 @@
 from api_collector import FreeCurrencyCollector
 from file_handle import FileHandler
-from cloud_storage import CSClient
+from gcloud import GClient
 from dotenv import load_dotenv, find_dotenv
 import os
 
 
 def start(request):
     load_dotenv(find_dotenv())
-    local_env = os.getenv("LOCAL")
-    collector = FreeCurrencyCollector()
-    api_data = collector.get_latest()
-
-    if local_env == "1":
+    if request == "Local":
+        secret = os.getenv("API_TOKEN")
+        collector = FreeCurrencyCollector(api_token=secret)
+        api_data = collector.get_latest()
         file_explore = FileHandler()
         file_explore.save_json(api_data)
     else:
-        cloud_storage_client = CSClient()
+        google_cloud = GClient()
+        project_id = os.getenv("PROJECT_ID")
+        secret_id = os.getenv("SECRET_ID")
+        version_id = os.getenv("VERSION_ID")
+        secret = google_cloud.access_secret(project_id, secret_id, version_id)
+        collector = FreeCurrencyCollector(api_token=secret)
+        api_data = collector.get_latest()
         currency = collector.base_currency
-        path = cloud_storage_client.create_path(currency)
-        destination = f"{currency}/{path}"
-        return cloud_storage_client.send_json_object(
+        path = google_cloud.create_path(currency)
+        google_cloud.send_json_object(
             api_data,
-            destination=destination,
+            destination=path,
             bucket_name='free_currency_exchange_data'
         )
+    return print('End run')
 
 
 if __name__ == "__main__":
-    start({})
+    start("Remoto")
